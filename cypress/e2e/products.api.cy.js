1beb369b0818aa2cbe748793c1928e5cb14e81ab
Vinicius M <vini.gm.05@gmail.com>
2025-10-17 20:35:24 -0300

    fisrt test

--- cypress/e2e/products.api.cy.js
+++ cypress/e2e/products.api.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Funcionalidade: Produtos', () => {
+  it('Deve retornar status 200 e um objeto com os produtos ', () => {
+    cy.request('GET', '/products') .then(response => {
+      cy.expect(response.status).to.eq(200);
+      cy.expect(response.body).to.be.an('object');
+
+      cy.expect(response.body).to.have.property('products');
+      cy.expect(response.body.products).to.be.an('array').and.have.lengthOf(30);
+
+      cy.expect(response.body).to.have.property('total', 194);
+    })
   })
 })
